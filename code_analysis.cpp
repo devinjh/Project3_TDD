d63ee68ce44d008a541f313a9cd792d4fdc8483f
Hopkins <djh155@ules-imac-09.uanet.edu>
2018-11-16 18:09:46 -0500

    Rule 1, distinguish between source archives and non-source archives

---
+++ code_analysis.cpp
@@ -0,0 +1,42 @@
+/*
+  @file code_analysis.cpp
+
+  Implementation of analysis requests
+*/
+
+#include "code_analysis.hpp"
+#include "get_language_from_filename.hpp"
+#include "xmlWrapper.hpp"
+
+#include <string>
+
+/** Generate source analysis XML based on the request
+ * Content is wrapped with an XML element that includes the metadata
+ * @param request Data that forms the request
+ * @retval Source analysis request in XML format
+ * @retval Empty string if invalid
+ */
+std::string formatAnalysisXML(const analysis_request& request) {
+
+    // Code for Rule #1
+    if (request.disk_filename != "")
+    {
+    	if (request.disk_filename.find(".zip") != -1 || request.disk_filename.find(".gz") != -1)
+    	{
+    		return request.entry_filename;
+    	}
+    	else
+    	{
+    		return request.disk_filename;
+    	}
+    }
+
+    // wrap the content with a unit element
+    xmlWrapper wrap("code", "http://mlcollard.net/code");
+    wrap.startElement("unit");
+    wrap.attribute("language", request.option_language);
+    wrap.content(request.source_code);
+    wrap.endElement();
+
+    return wrap.xml();
+}
